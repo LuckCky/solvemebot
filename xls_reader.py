a889e3563cecf9d594a9c8c42171e32f77fcd4f1
LuckCky <www.aivanov@gmail.com>
2017-01-22 23:55:35 +0300

    adding questions to db

--- xls_reader.py
+++ xls_reader.py
@@ -3,8 +3,31 @@ import xlrd
 rb = xlrd.open_workbook('q/q.xls', formatting_info=True)
 sheet = rb.sheet_by_name('Данные')
 
-for rownum in range(sheet.nrows):
+user_id = 1
+
+import sqlite3
+
+import conf
+
+connection = sqlite3.connect(conf.storage_name_params)
+cursor = connection.cursor()
+
+
+for rownum in range(1, sheet.nrows):
     row = sheet.row_values(rownum)
     print(row)
-    for c_el in row:
-        print(c_el)
+    correct_answers = row[-2].lower().split('\n')
+    cursor.execute(conf.insert_questions, (user_id, '2017-01-22', row[0], row[1], row[2], row[3]))
+
+    # if row[2].startswith('/'):
+        # print('I GOT PIC HERE')
+    # print(answers)
+print(correct_answers)
+print(len(correct_answers))
+
+if 'плющ плащ' in correct_answers:
+    correct_answers.remove('плющ плащ')
+
+print(correct_answers)
+print(len(correct_answers))
+connection.commit()
