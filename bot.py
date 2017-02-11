4b49269029f5cc45707b62a15a073fc0bfb96cff
LuckCky <www.aivanov@gmail.com>
2017-02-11 15:03:13 +0300

    cherrypy server engaged

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import telebot
 import cherrypy
+import time
 
 from utils import working_hours, insert_questions, read_current_question
 from utils import create_table, change_correct_answers, read_next_question
@@ -104,39 +105,45 @@ def answer_reaction(message):
 # def callback_inline(call):
 
 
-# class WebhookServer(object):
-#     @cherrypy.expose
-#     def index(self):
-#         if 'content-length' in cherrypy.request.headers and \
-#                         'content-type' in cherrypy.request.headers and \
-#                         cherrypy.request.headers['content-type'] == 'application/json':
-#             length = int(cherrypy.request.headers['content-length'])
-#             json_string = cherrypy.request.body.read(length).decode("utf-8")
-#             update = telebot.types.Update.de_json(json_string)
-#             bot.process_new_updates([update])
-#             return ''
-#         else:
-#             raise cherrypy.HTTPError(403)
+class WebhookServer(object):
+    @cherrypy.expose
+    def index(self):
+        if 'content-length' in cherrypy.request.headers and \
+                        'content-type' in cherrypy.request.headers and \
+                        cherrypy.request.headers['content-type'] == 'application/json':
+            length = int(cherrypy.request.headers['content-length'])
+            json_string = cherrypy.request.body.read(length).decode("utf-8")
+            update = telebot.types.Update.de_json(json_string)
+            bot.process_new_updates([update])
+            return ''
+        else:
+            raise cherrypy.HTTPError(403)
+
+WEBHOOK_PORT = 443
+WEBHOOK_LISTEN = '0.0.0.0'
+
+WEBHOOK_URL_BASE = 'https://safe-savannah-14774.herokuapp.com'
+WEBHOOK_URL_PATH = "/{}/".format(conf.token)
 
 
 if __name__ == "__main__":
     bot.remove_webhook()
-    # time.sleep(5)
-    # bot.set_webhook(url=WEBHOOK_URL_BASE + WEBHOOK_URL_PATH)
-    #
-    # cherrypy.config.update({
-    #     'engine.autoreload.on': False,
-    #     'server.socket_host': WEBHOOK_LISTEN,
-    #     'server.socket_port': WEBHOOK_PORT,
-    #     'server.ssl_module': 'builtin',
-    #     'server.ssl_certificate': WEBHOOK_SSL_CERT,
-    #     'server.ssl_private_key': WEBHOOK_SSL_PRIV
-    # })
-    #
-    # # RUN SERVER, RUN!
-    # cherrypy.tree.mount(WebhookServer(), WEBHOOK_URL_PATH, {'/': {}})
+    time.sleep(5)
+    bot.set_webhook(url=WEBHOOK_URL_BASE + WEBHOOK_URL_PATH)
+
+    cherrypy.config.update({
+        'engine.autoreload.on': False,
+        'server.socket_host': WEBHOOK_LISTEN,
+        'server.socket_port': WEBHOOK_PORT,
+        # 'server.ssl_module': 'builtin',
+        # 'server.ssl_certificate': WEBHOOK_SSL_CERT,
+        # 'server.ssl_private_key': WEBHOOK_SSL_PRIV
+    })
+
+    # RUN SERVER, RUN!
+    cherrypy.tree.mount(WebhookServer(), WEBHOOK_URL_PATH, {'/': {}})
     # cherrypy.tree.mount(GetBankPost(), WEBHOOK_URL_PATH + conf.bank_token + '/', {'/': {}})
-    #
-    # cherrypy.engine.start()
-    # cherrypy.engine.block()
-    bot.polling()
+
+    cherrypy.engine.start()
+    cherrypy.engine.block()
+    # bot.polling()
