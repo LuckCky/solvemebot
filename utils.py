b3453f06c74fa614889f28a4bb46ecaec4474f51
LuckCky <www.aivanov@gmail.com>
2017-02-20 23:01:59 +0300

    debugging

--- utils.py
+++ utils.py
@@ -57,8 +57,12 @@ def insert_questions(user_id):
     for rownum in range(1, sheet.nrows):
         row = sheet.row_values(rownum)
         # correct_answers = row[-2].lower().split('\n')
-        cursor.execute(conf.insert_questions, (user_id, date, row[0], row[1], row[2], row[3]))
-    connection.commit()
+        try:
+            cursor.execute(conf.insert_questions, (user_id, date, row[0], row[1], row[2], row[3]))
+        except Exception as e:
+            return str(e)
+        finally:
+            connection.commit()
 
 
 def read_current_question(user_id):
